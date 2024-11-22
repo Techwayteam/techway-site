81f0e8f9d43578a64c6563fe377bdfa4662ae91d
Techwayteam <leonardodefreitasreis@gmail.com>
2024-11-22 11:00:35 -0300

    Update PageData.ts

--- types/PageData.ts
+++ types/PageData.ts
@@ -2,33 +2,33 @@ import * as LucideIcons from 'lucide-react';
 
 export type PageData = {
   seo: {
-    pageTitle: string;
-    pageDescription: string;
-    pageKeywords: string;
+    pageTitle: string,
+    pageDescription: string,
+    pageKeywords: string,
   }
   hero: {
-    heroTitle: string;
-    heroSubtitle: string;
-    heroCTA: string;
-    heroImage: string;
+    heroTitle: string,
+    heroSubtitle: string,
+    heroCTA: string,
+    heroImage: string,
   }
   story: {
-    storyTitle: string;
-    storyText: string;
-    storyImage: string;
+    storyTitle: string,
+    storyText: string,
+    storyImage: string,
   }
   benefits: Array<{
-    title: string;
-    description: string;
-    icon: keyof typeof LucideIcons;
+    title: string,
+    description: string,
+    icon: keyof typeof LucideIcons,
   }>
   solution: {
-    solutionTitle: string;
-    solutionText: string;
+    solutionTitle: string,
+    solutionText: string,
   }
   cta: {
-    ctaText: string;
-    ctaButton: string;
-    ctaLink: string;
+    ctaText: string,
+    ctaButton: string,
+    ctaLink: string,
   }
 }
