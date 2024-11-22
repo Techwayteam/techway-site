d737b13bab326b68fdfdf8e6342832d973b5e2ac
Techwayteam <leonardodefreitasreis@gmail.com>
2024-11-22 09:49:04 -0300

    Create PageData.ts

---
+++ types/PageData.ts
@@ -0,0 +1,34 @@
+import { type LucideIcon } from 'lucide-react'
+
+export type PageData = {
+  seo: {
+    pageTitle: string
+    pageDescription: string
+    pageKeywords: string
+  }
+  hero: {
+    heroTitle: string
+    heroSubtitle: string
+    heroCTA: string
+    heroImage: string
+  }
+  story: {
+    storyTitle: string
+    storyText: string
+    storyImage: string
+  }
+  benefits: Array<{
+    title: string
+    description: string
+    icon: keyof typeof import('lucide-react')
+  }>
+  solution: {
+    solutionTitle: string
+    solutionText: string
+  }
+  cta: {
+    ctaText: string
+    ctaButton: string
+    ctaLink: string
+  }
+}
