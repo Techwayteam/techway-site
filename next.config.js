d003e0a2cbcc266cef1009df14b1c293f79b77c1
Techwayteam <leonardodefreitasreis@gmail.com>
2024-11-25 11:54:06 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,26 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: '',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, 'components'),
+      '@styles': path.resolve(__dirname, 'styles'),
+    };
+
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      };
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
